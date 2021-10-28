4324e1152bc31d6ea7c51087a1cd7ecc8ed34884
moisesoliveira08 <89934509+moisesoliveira08@users.noreply.github.com>
2021-10-28 11:26:22 -0300

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -10,7 +10,7 @@ int main(){
     long int rev = 0;
     long int soluc = 0;
     for(i=999; i>99; i--){
-        for(j=999; j>99; j--){
+        for(j=999; j>99; j--){ // pega todos os produtos possiveis entre 2 numeros de 3 digitos
             num = i*j;
             aux = num;
             rev = 0;
@@ -19,7 +19,7 @@ int main(){
                  aux = aux / 10;
             }
             
-            if ((rev == num) && num > soluc)
+            if ((rev == num) && num > soluc) // roda até encontrar o maior possivel
                 soluc = num;
             
         } 
